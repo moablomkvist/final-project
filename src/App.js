eb6a8b2c97ff1ed21ea64edbeceecb42010b4922
Moa Blomkvist <moa@birgerochblom.se>
2021-01-19 16:15:44 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { AllPatterns } from './components/AllPatterns'
 
 export const App = () => {
   return (
     <div>
+      <AllPatterns/>
       Find me in src/app.js!
     </div>
   )
---
+++ src/components/AllPatterns.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const READ_ONLY_URL = 'https://api.ravelry.com/read-43a3981546776de4df6fbb23599aa0a2.json'
+
+export const AllPatterns = () => {
+return (
+  <p>Hej</p>
+)
+    
+}

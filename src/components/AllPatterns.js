cb570ff966e14b928ea1a1a74441b51fd2187e36
Moa Blomkvist <moa@birgerochblom.se>
2021-01-20 10:55:40 +0100

    api-url

--- src/components/AllPatterns.js
+++ src/components/AllPatterns.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const READ_ONLY_URL = 'https://api.ravelry.com/read-43a3981546776de4df6fbb23599aa0a2.json'
+const READ_ONLY_URL = 'https://api.ravelry.com/pattern_categories/list.json'
 
 export const AllPatterns = () => {
 return (

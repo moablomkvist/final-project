45cf70d78baba45134ffa738dec0f31b632c7c06
Moa Blomkvist <moa@birgerochblom.se>
2021-01-24 22:55:00 +0100

    style

--- src/components/RowCounter.js
+++ src/components/RowCounter.js
@@ -3,17 +3,16 @@ import React, { useState } from 'react'
 export const RowCounter = () => {
   const [clickCount, setClickCount] = useState(0);
 
-  const handleClicked = (numClicks) => {
+  const onClicked = (numClicks) => {
     setClickCount(clickCount + numClicks)
   };
 
-
   return (
     <section className="counter-container">
-      <button onClick={() => handleClicked(1)}>+1</button>
-      <button onClick={() => handleClicked(-1)}>-1</button>
-      <p className="counter-total">Knitted rows: {clickCount}</p>
-      {clickCount > 9 ? <p>Yay! You getting there</p> : <></>}
+      <button className="counter-button" onClick={() => onClicked(1)}>+ 1</button>
+      <button className="counter-button" onClick={() => onClicked(-1)}>- 1</button>
+      <p className="counter-total">KNITTED ROWS: {clickCount}</p>
+      {clickCount > 9 ? <p>Yay! You're getting there</p> : <></>}
     </section>
   )
 }

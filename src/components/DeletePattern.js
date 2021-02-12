69e879ccfa814401cb78da5eec5d9078a53d472d
Moa Blomkvist <moa@birgerochblom.se>
2021-02-12 11:17:06 +0100

    cleaning

---
+++ src/components/DeletePattern.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import { FavouriteDeleteButton } from "styling/lib/FavouriteDeleteButton"
+
+export const DeletePattern = ({ pattern }) => {
+const accessToken = useSelector((store) => store.userReducer.login.accessToken);
+
+let history = useHistory();
+const handleDeleteSuccess = () => {
+  history.go();
+};
+
+  const handleDeletePattern = (_id) => {
+    fetch(`https://knitting-circle.herokuapp.com/patterns/${_id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: accessToken,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Could not delete pattern");
+        }
+        return res.json()
+      })
+      .then((json) => handleDeleteSuccess(json))
+      .catch((err) => console.log("error:", err));
+  };
+
+  return (
+    <FavouriteDeleteButton onClick={() => {handleDeletePattern(pattern._id);}}>
+      <img src="../assets/trash-2.svg" alt="trash-delete"/>
+    </FavouriteDeleteButton>
+  )
+}
